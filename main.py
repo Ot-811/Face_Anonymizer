f35c0189859d0c2e08ab99975df5683d1b154161
Ot-811 <omtr0904@gmail.com>
2026-05-20 21:39:39 +0530

    simple setup, works for image only rn

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2
+import mediapipe as mp
+
+image_path = './data/face1.jpg'
+
+img  = cv2.imread(image_path)
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+for (x, y, w, h) in faces:
+    face = img[y:y+h, x:x+w]
+
+    blurred_face = cv2.GaussianBlur(face, (99, 99), 20)
+
+    img[y:y+h, x:x+w] = blurred_face
+
+cv2.imshow("Anonymized Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
